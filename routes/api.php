28f9d67c6fa43b9b6dca64b38421c0d0cde300f1
enyx <tnhan20152016@gmail.com>
2025-05-18 22:53:09 +0700

    add api/check-score route

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScoreController;
-
 Route::get('/check-score', [ScoreController::class, 'checkScore']);
+Route::get('/hello', function () {
+    return response()->json(['message' => 'Hello world']);
+});
