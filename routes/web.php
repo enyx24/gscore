8a8c6c02865f524b1a154ced70976a881be57d6e
enyx <tnhan20152016@gmail.com>
2025-05-19 22:17:45 +0700

    rename routes

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-    return 'Test route working!';
-});
+// Route::get('/test', function () {
+//     return 'Test route working!';
+// });
 Route::get('/', function () {
     return view('dashboard');
 });
 Route::get('/check', fn () => view('check'));
-Route::get('/stats', fn () => view('stats'));
-Route::get('/top-a', fn () => view('top_a'));
+Route::get('/report', fn () => view('report'));
+// Route::get('/top-a', fn () => view('top_a'));
