ad1d4a353efce41a1e6c01b23bb6cbdae43f146e
enyx <tnhan20152016@gmail.com>
2025-05-19 20:16:22 +0700

    fix error messages

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -12,20 +12,20 @@ class ScoreController extends Controller
         // VALIDATION
         if (!$reg) {
             return response()->json([
-                'error' => 'Missing reg query parameter'
+                'error' => 'Missing registration number'
             ], 400);
         }
         // https://stackoverflow.com/questions/18683444/detect-if-a-string-contains-any-numbers
         if (!ctype_digit($reg) || strlen($reg) != 8)
             return response()->json([
-                'error' => 'reg is not a valid id'
+                'error' => 'Registration number is not valid'
             ], 400);
         
         // QUERY
         $scores = Score::where('uid', $reg)->get();
         if ($scores->isEmpty())
             return response()->json([
-                'error' => 'ID not found',
+                'error' => 'Registration number not found',
                 'debug' => "Received ID: $reg",
             ], 404);
         return response()->json($scores);
