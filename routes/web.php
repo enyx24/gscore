2002c780186c0120e0fe7c8b525934162f6cc6fe
enyx <tnhan20152016@gmail.com>
2025-05-19 20:18:33 +0700

    config tailwind

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', function () {
     return 'Test route working!';
 });
+Route::get('/', function () {
+    return view('dashboard');
+});
+Route::get('/check', fn () => view('check'));
+Route::get('/stats', fn () => view('stats'));
+Route::get('/top-a', fn () => view('top_a'));
